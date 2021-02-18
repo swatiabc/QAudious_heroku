d0d12abec33748045ad3bc23556a46f828d11cf2
swatiabc <46042312+swatiabc@users.noreply.github.com>
2021-02-18 20:24:18 +0530

    make it better

--- qaudious/settings.py
+++ qaudious/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'templates')
 
 STATIC_URL = '/templates/'
 

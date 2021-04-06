3cd577267633db9571c9b4128909b15af5bf8974
swatiabc <46042312+swatiabc@users.noreply.github.com>
2021-04-07 01:29:25 +0530

    changes

--- qaudious/urls.py
+++ qaudious/urls.py
@@ -22,7 +22,12 @@ from question_answer.views import(question_page,qa_display_page,qa_saved_page,po
 from users.views import(signin_home_page,signin_form_page,register_home_page,
             register_form_page,dashboard_page,index,)
 
+from profiles.views import (profile_page,)
+
 urlpatterns = [
+    path('profile.html',profile_page),
+
+
     path('index.html',index),
     path('signin_form.html',signin_form_page),
     path('signin_home.html',signin_home_page),
